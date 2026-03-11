c73d1c56d6e01fc4cb049024ced7415983e2fcef
Gianni Strano <Gianni.Strano@amexgbt.com>
2026-03-11 15:54:33 +0000

    Move connection verification comment to Program.cs

--- EnsekMeterReadingAPI/Program.cs
+++ EnsekMeterReadingAPI/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 // Established connection to GitHub: https://github.com/StranoGianni/MeterReadingAPI
+// Comment that verify the connection
 namespace EnsekMeterReadingAPI
 {
     public class Program
